9b1f3b53c52d6f02a06d0899efa33d7209c79574
Siddharth <siddharth@vasantsoftwaressolution.com>
2023-10-04 19:58:42 +0530

    jkhkugkghghgjhg

--- errors/customErrors.js
+++ errors/customErrors.js
@@ -3,24 +3,7 @@ let { StatusCodes } =  require('http-status-codes');
  class NotFoundError extends Error {
   constructor(message) {
     super(message);
-    this.name = 'NotFoundError';
-    this.statusCode = StatusCodes.NOT_FOUND;
-  }
-}
-class BadRequestError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'BadRequestError';
-    this.statusCode = StatusCodes.BAD_REQUEST;
-  }
-}
-class UnauthenticatedError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'UnauthenticatedError';
-    this.statusCode = StatusCodes.UNAUTHORIZED;
-  }
-}
+    t
  class UnauthorizedError extends Error {
   constructor(message) {
     super(message);
